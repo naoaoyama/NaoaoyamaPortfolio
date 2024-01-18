fcc8c4f98a581cfd470969246546c585f0b90c07
Nao Aoyama <ea80002@gmail.com>
2024-01-18 11:15:55 +0900

    swiper was deleted

--- JS/main.js
+++ JS/main.js
@@ -25,26 +25,5 @@
         reset: true,
       });
 
- {
-  const swiper = new Swiper('.swiper', {
-    loop:true,
-    initialSlide: 0,
-    speed: 1000,
-    effect:"coverflow",
-    slidesPerView:3,
-    coverflowEffect: {
-      // rotate: 50,
-      // stretch: 0,
-      // depth: 100,
-      // modifier: 1,
-      slideShadows: false
-    },
-    pagination: {
-      el:".swiper-pagination"
-    },
-    navigation: {
-      nextEl:".swiper-button-next",
-      prevEl:".swiper-button-prev"
-    }
-  });
-}
+ 
+
